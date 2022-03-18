828373b26f62f0ebdcc850a70584f85e9b2e5476
frastsytt <frastsytt@gmail.com>
2022-03-18 13:00:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());        // Avoid CORS errors in browsers
+app.use(express.json()) // Populate req.body
+
+const pets = [
+    { id: 1, name: "Cizzbor", sex: "male", species: "cat" },
+    { id: 2, name: "Woowo", sex: "female", species: "dog" },
+    { id: 3, name: "Crazlinger", sex: "male", species: "cat" }
+]
+
+app.get('/pets', (req, res) => {
+    res.send(pets)
+})
+
+app.get('/pets/:id', (req, res) => {
+    if (typeof pets[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({ error: "Pet not found" })
+    }
+    res.send(pets[req.params.id - 1])
+})
+
+app.post('/pets', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({ error: 'One or all params are missing' })
+    }
+    let newPet = {
+        id: pets.length + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+    pets.push(newPet)
+    res.status(201).location('localhost:8080/pets/' + (pets.length - 1)).send(
+        newPet
+    )
+})
+
+app.listen(8080, () => {
+    console.log(`API up at: http://localhost:8080`)
+})
