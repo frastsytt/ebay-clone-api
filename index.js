87dc370e5101c4de99cd496f9f857b4bddd48db3
frastsytt <frastsytt@gmail.com>
2022-04-01 10:38:52 +0300

    As a user I can authenticate myself to the app [Delivers #181676569]

--- index.js
+++ index.js
@@ -2,20 +2,55 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+var crypto = require('crypto');
+const fs = require('fs');
 
 
 app.use(cors());        // Avoid CORS errors in browsers
 app.use(express.json()) // Populate req.body
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+var generate_key = function() {
+    // 16 bytes is likely to be more than enough,
+    // but you may tweak it to your needs
+    return crypto.randomBytes(16).toString('base64');
+};
+
 const pets = [
-    { id: 1, name: "Cizzbor", sex: "male", species: "cat" },
+    { id: 1, name: "Cizzbor", sex: "male", species: "cat"},
     { id: 2, name: "Woowo", sex: "female", species: "dog" },
-    { id: 3, name: "Crazlinger", sex: "male", species: "cat" }
+    { id: 3, name: "Crazlinger", sex: "male", species: "cat", image: ""}
 ]
+
+const users = [
+    { id: 0, username: "root", password: "admin"}
+]
+
+app.post('/', (req, res) => {
+    const result = users.find( ({ username }) => username === 'root' );
+    console.log(result)
+    console.log(result.username)
+    console.log(result.password)
+    console.log(req.query.password)
+    console.log(req.query.username)
+    if (!result || !(result.password === req.query.password)) {
+        return res.status(401).json({error: 'Invalid username/password'})
+    }
+
+    var sess_id = generate_key()
+    console.log(sess_id)
+    let data = ""
+    fs.writeFile(`.\\sessions\\${sess_id}`, data, (err) => {
+        if (err)
+            console.log(err);
+    });
+    res.cookie('session_id', sess_id)
+})
 
 app.get('/pets', (req, res) => {
     res.send(pets)
@@ -28,6 +63,7 @@ app.get('/pets/:id', (req, res) => {
     if (!req.params.id) {
         return res.status(400).send({ error: 'One or all params are missing' })
     }
+
     res.send(pets[req.params.id - 1])
 })
 
